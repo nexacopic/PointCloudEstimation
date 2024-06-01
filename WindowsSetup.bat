787625ed26d1c45c5467d3565488d84202ddabf4
skizo <142146272+nexacopic@users.noreply.github.com>
2024-06-01 13:30:36 +0100

    fix some issues

--- WindowsSetup.bat
+++ WindowsSetup.bat
@@ -1,8 +1,6 @@
 @echo off
 echo Installing packages
 pip install -r requirements.txt
-echo Cleaning up any loose files
-./Clean.bat
-
+cd py
 echo Building pointman
 python setup.py build_ext --inplace
